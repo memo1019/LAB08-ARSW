3120f17442186ab791a6c278a571c1b5bfe9b768
Federico Barrios <federico.barrios@mail.escuelaing.edu.co>
2021-03-25 18:39:21 -0500

    Parte II

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,3 +1,5 @@
+/* global Stomp */
+
 var app = (function () {
 
     var stompClient = null;
@@ -8,8 +10,8 @@ var app = (function () {
         if (connected) {
             $("#pointslist").show();
             $("#canvas").show();
-        }
-        else {
+            drawFromCanvas();
+        } else {
             $("#pointslist").hide();
             $("#canvas").hide();
         }
@@ -37,31 +39,47 @@ var app = (function () {
         console.log("Disconnected");
     }
 
-    function sendPoint() {
-        stompClient.send("/app/hello", {}, JSON.stringify(
-                {
-                    'x': $("#x").val(),
-                    'y': $("#y").val()
-                }
-        ));
+    function drawFromCanvas() {
+        var canvas = document.getElementById("canvas");
+        var xPos = $("#canvas").offset().left + window.screenX;
+        var yPos = $("#canvas").offset().top + window.screenY;
+        if (window.PointerEvent) {
+            canvas.addEventListener("pointerdown", function (event) {
+                stompClient.send("/app/hello", {}, JSON.stringify(
+                        {
+                            'x': event.pageX - xPos,
+                            'y': event.pageY - yPos
+                        }
+                ));
+            });
+        } else {
+            canvas.addEventListener("mousedown", function (event) {
+                stompClient.send("/app/hello", {}, JSON.stringify(
+                        {
+                            'x': event.pageX - xPos,
+                            'y': event.pageY - yPos
+                        }
+                ));
+            });
+        }
     }
-    
-    function addPointToCanvas(point) {        
+
+    function addPointToCanvas(point) {
         var canvas = document.getElementById("canvas");
         var ctx = canvas.getContext("2d");
         ctx.beginPath();
         ctx.arc(point.x, point.y, 3, 0, 2 * Math.PI);
         ctx.stroke();
-    };
-    
+    }
+    ;
+
     function showPoints(point) {
-        $("#points").append("<tr><td>" + 
+        $("#points").append("<tr><td>" +
                 "{x: " + point.x + ", y: " + point.y + "}" + "</td></tr>");
     }
 
     return {
         connect: connect,
-        disconnect: disconnect,
-        sendPoint: sendPoint
+        disconnect: disconnect
     };
 })();
